74c2055e64a5aa39cf4e5eaee95ef8478222888c
usman42674 <150067286+usman42674@users.noreply.github.com>
2023-11-17 16:56:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,21 +2,21 @@ const fs = require('fs-extra')
 if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
 
 
-//═══════[Required Variables]════════\\
+//═══════[𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/yasiyaofc1/FORZEN-MD'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94760018802'
+global.github = 'https://github.com/usman42674/FORZEN'
+global.location = 'Rawalpindi'
+global.gurl = 'https://instagram.com/usman42677?igshid=aDk2a2p5NG1yOWU0
+global.sudo = process.env.SUDO || 923140053156'
 global.devs = '94760018802';
-global.website = 'https://github.com/yasiyaofc1/FORZEN-MD' //wa.me/+94760018802
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/0b5a7839e8ee21874921e.jpg'
+global.website = 'https://github.com/usman42674/FORZEN-MD' //wa.me/+94760018802
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://photos.app.goo.gl/iPQPPco93Q9skt3MA'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'FORZEN MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'YASIYA OFC' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𓄂≛͙⃝͙𝑴𝒓 𝑴𝒂𝒏𝒊 MD' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𓄂≛͙⃝͙𝑴𝒓 𝑴𝒂𝒏𝒊 OFC' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'YASIYA OFC' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
